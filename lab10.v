e70725310f93baab78c96f71f1057079f595670f
samcorrell <124624844+samcorrell@users.noreply.github.com>
2024-12-03 11:44:39 -0500

    Add files via upload

--- lab10.v
+++ lab10.v
@@ -21,8 +21,19 @@
 
 
 module lab10(
-
+    input clk, rst,
+    input [2:0] addr1, addr2,ram_addr,
+    output [7:0] ram_out
     );
+    wire w_rf, DA, SA, SB, w_ram;
+    wire [2:0] adr, st_out;
+    wire [3:0] rom_d_out, A, B;
+    wire [7:0] mult_out;
+    cu uut(clk,rst,addr1, addr2, w_rf, adr, DA, SA, SB, st_out, w_ram);
+    rom uut1(adr,rom_d_out);
+    RF uut2(A, B, SA, SB, rom_d_out, DA, w_rf, rst, clk);
+    comb_mult uut3(A, B, mult_out);
+    ram uut4(w_ram, clk, rst, mult_out, ram_addr, ram_out);
 endmodule
 
 
@@ -149,5 +160,124 @@ begin
    endcase
 end
 endmodule
+
+
+
+
+module cu ( 
+
+input clk, reset,
+  input [2:0] adr1,
+  input [2:0] adr2,
+  output reg w_rf,
+  output reg [2:0] adr,
+  output reg DA,  SA,SB,
+  output reg [2:0] st_out,
+  //output reg done,
+  output reg w_ram
+);
+
+   
+  
+parameter S0_idle = 0 , S1_send_adr1 = 1 , S2_send_adr2 = 2 ,S3_multiply = 3 ,S4_write_ram = 4,S5_read_ram=5 ;
+reg [2:0] PS,NS ;
+
+    always@(posedge clk or posedge reset)
+        begin
+            if(reset)
+                PS <= S0_idle;   
+            else    
+                PS <= NS ;
+        end  
+
+
+		  
+
+    always@(*)
+        begin 
+            
+            case(PS)
+				S0_idle:begin
+				NS = S1_send_adr1;
+                                w_rf <=1'b1;
+                                w_ram <=1'b1;
+            st_out <= 3'b000;
+				end
+				
+				S1_send_adr1:begin	
+				w_rf <=1'b1;
+				adr<=adr1;
+				DA <=1'b0;
+				SA <=1'b0;
+				SB <=1'b1;
+				st_out <= 3'b001;
+				NS = S2_send_adr2;
+				end
+				
+				S2_send_adr2:begin
+				w_rf <=1'b1;
+				adr<=adr2;
+				NS = S3_multiply;
+				DA <=1'b1;
+				SA <=1'b0;
+				SB <=1'b1;
+            st_out <= 3'b010;
+			   end
+			
+		                S3_multiply: begin
+				NS = 	S4_write_ram;
+                                st_out <= 3'b011;
+				w_ram<=1;
+			        end
+
+                                S4_write_ram: begin
+                                st_out <= 3'b100;
+				NS = 	S5_read_ram;
+				end
+				
+				S5_read_ram: begin
+                                w_ram<=0;
+                                //done <=1;
+                                st_out <= 3'b101;
+				if(!reset) begin
+				NS = 	S5_read_ram;
+				end
+				else begin
+				NS = S0_idle;
+				end
+				end
+				endcase
+				end
+				
+			
+
+endmodule
+
+
+module comb_mult(
+    input [3:0] a, b,
+    output [7:0] p
+    );
+    wire [0:0] a0b0, a1b0, a2b0, a3b0, a0b1, a1b1, a2b1, a3b1, a0b2, a1b2, a2b2, a3b2, a0b3, a1b3, a2b3, a3b3;
+    wire[7:0] pp1, pp2, pp3, pp4;
+    assign a0b0 = a[0]*b[0]; assign a1b0 = a[1]*b[0]; assign a2b0 = a[2]*b[0]; assign a3b0 = a[3]*b[0]; 
+    assign a0b1 = a[0]*b[1]; assign a1b1 = a[1]*b[1]; assign a2b1 = a[2]*b[1]; assign a3b1 = a[3]*b[1];  
+    assign a0b2 = a[0]*b[2]; assign a1b2 = a[1]*b[2]; assign a2b2 = a[2]*b[2]; assign a3b2 = a[3]*b[2]; 
+    assign a0b3 = a[0]*b[3]; assign a1b3 = a[1]*b[3]; assign a2b3 = a[2]*b[3]; assign a3b3 = a[3]*b[3];   
+    
+    assign pp1 = {4'b0, a3b0, a2b0, a1b0, a0b0};
+    assign pp2 = {3'b0, a3b1, a2b1, a1b1, a0b1, 1'b0};
+    assign pp3 = {2'b0, a3b2, a2b2, a1b2, a0b2, 2'b0};
+    assign pp4 = {1'b0, a3b3, a2b3, a1b3, a0b3, 3'b0};
+    
+    
+    assign p = pp1+pp2+pp3+pp4;
+    
+endmodule
+
+
+
+
+
 
     
